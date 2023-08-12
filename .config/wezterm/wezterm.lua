5e8218cc1474c84135d2946c6db2185bba5e5a0d
Evan Hastings <56352068+evanrh@users.noreply.github.com>
2023-08-12 15:26:40 -0400

    Added basic windows config for Wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -8,7 +8,17 @@ end
 
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  -- Do Windows things
+  -- Setup launch menu for a Windows config
+  config.launch_menu = {
+    {
+      label = 'Powershell',
+      args = 'powershell.exe'
+    },
+    {
+      label = 'Command Prompt',
+      args = 'cmd.exe'
+    }
+  }
 end
 config.color_scheme = 'Tokyo Night Storm'
 
