f4d3ab2c2a7149335e626ceac18249c6cd445401
Evan Hastings <56352068+evanrh@users.noreply.github.com>
2023-08-15 10:11:16 -0400

    Fixed Wezterm maximize and added Deno config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,6 @@ end
 alias config="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 alias vi="nvim"
 alias vim="nvim"
+
+export DENO_INSTALL="/home/evanrh/.deno"
+export PATH="$DENO_INSTALL/bin:$PATH"
